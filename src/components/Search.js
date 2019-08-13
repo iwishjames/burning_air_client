1beeb9a34ab2371ce571c3f59c0b17beb355a6af
Pakaros Pumanee <pakarospumanee@Pakaross-MacBook.local>
2019-08-13 17:52:55 +1000

    add components

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import import React, { Component } from 'react';
+
+class Seacrh extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Burning Airline</h1>
+                <SeacrhForm />
+                <SeacrhResult />
+            </div>
+
+        );
+    }
+}
+export default Seacrh;
