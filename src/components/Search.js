edbc6588fcd663f301cbccdece3dac2a805ed08e
Pakaros Pumanee <pakarospumanee@Pakaross-MacBook.local>
2019-08-13 18:58:32 +1000

    Search Form

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,50 @@
-import import React, { Component } from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-class Seacrh extends Component {
+class Search extends Component {
     render() {
         return(
             <div>
-                <h1>Burning Airline</h1>
-                <SeacrhForm />
-                <SeacrhResult />
+                <h1>Crapping Airline</h1>
+                <SearchForm />
+                <SearchResult />
             </div>
 
         );
     }
 }
-export default Seacrh;
+
+class SearchForm extends Component {
+    constructor() {
+        super();
+        this.state = {from: '', to: ''}
+        this._handleInput = this._handleInput.bind( this );
+        this._handleSubmit = this._handleSubmit.bind( this );
+    }
+    _handleInput(event) {
+        this.setState({from: event.target.value})
+        this.setState({to: event.target.value})
+    }
+    _handleSubmit(event) {
+        event.preventDefault();
+    }
+    render() {
+        return(
+            <form onSubmit={ this._handleSubmit }>
+                <input type="text" placeholder="from" required onInput={ this._handleInput } />
+                <input type="text" placeholder="to" required onInput={ this._handleInput } />
+                <input type="submit" ></input>
+            </form>
+        );
+    }
+}
+class SearchResult extends Component {
+    render() {
+        return(
+            <div>
+                <p>Flight Search Result coming</p>
+            </div>
+        );
+    }
+}
+export default Search;
