e17ec4d2ced37da9cfa6d01783c4441c727506d8
Lucy Ogden-Doyle <lucyod10@gmail.com>
2019-08-15 10:33:40 +1000

    Updates

---
+++ precache-manifest.995e5bf00871537165c93ab8a8df47c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a362e6b6ead1699cebbc23f35ea6c46",
+    "url": "/burning_air_client/index.html"
+  },
+  {
+    "revision": "d456547e6b235f29b09e",
+    "url": "/burning_air_client/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "846eb1925eff23f56610",
+    "url": "/burning_air_client/static/js/2.98cebf4e.chunk.js"
+  },
+  {
+    "revision": "d456547e6b235f29b09e",
+    "url": "/burning_air_client/static/js/main.8ab7b03f.chunk.js"
+  },
+  {
+    "revision": "117c369932152aa4c998",
+    "url": "/burning_air_client/static/js/runtime~main.ca0e0d25.js"
+  },
+  {
+    "revision": "e143c1987293c11fddf42983fe44d7f1",
+    "url": "/burning_air_client/static/media/chair_empty.e143c198.png"
+  },
+  {
+    "revision": "fa6ad40d248a9011005fb5609e17cf8d",
+    "url": "/burning_air_client/static/media/chair_filled.fa6ad40d.png"
+  }
+]);
